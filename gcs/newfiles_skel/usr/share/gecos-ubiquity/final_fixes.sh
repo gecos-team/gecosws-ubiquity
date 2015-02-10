c78fdfe801e888d3718c83258d48da0f0aad9fd5
David Amian <damian@emergya.com>
2015-02-10 14:57:12 +0100

    Removed certificate imports from final_fixes

--- gcs/newfiles_skel/usr/share/gecos-ubiquity/final_fixes.sh
+++ gcs/newfiles_skel/usr/share/gecos-ubiquity/final_fixes.sh
@@ -12,13 +12,3 @@ echo "#Sources in sources.list.d" > /target/etc/apt/sources.list
 
 chmod 644 /target/etc/apt/sources.list
 
-mkdir -p /target/usr/share/ca-certificates-imported/
-
-chmod 644 /target/usr/share/ca-certificates-imported/
-
-for cert in `ls /target/usr/share/ca-certificates/*/*`
-do
-    chroot /target keytool -cacert -keystore '/etc/java/cacerts-gcj' -file '$cert'
-    cp -a $cert /target/usr/share/ca-certificates-imported/
-done
-
